3d0b80201ce3b6c3db5c8291003520d23fbd0bea
Faiza Maliat <faiza.maliat20@gmail.com>
2025-08-12 21:42:26 +0600

    Complete routing for favorites

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,7 +1,7 @@
-import EmptyState from "../components/EmptyState";
-import ClientOnly from "../components/ClientOnly";
 import getCurrentUser from "../actions/getCurrentUser";
 import getFavoriteListings from "../actions/getFavoriteListing";
+import ClientOnly from "../components/ClientOnly";
+import EmptyState from "../components/EmptyState";
 import FavoritesClient from "./FavoritesClient";
 
 const ListingPage = async () => {
