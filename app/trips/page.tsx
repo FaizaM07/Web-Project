ce1c5e035aefa8cf4eb8b6feb0d8b5445a0f7ce8
Faiza Maliat <faiza.maliat20@gmail.com>
2025-08-26 01:06:42 +0600

    Connect with reservation

--- app/trips/page.tsx
+++ app/trips/page.tsx
@@ -1,9 +1,47 @@
+import getCurrentUser from "../actions/getCurrentUser";
+import getReservations from "../actions/getReservation";
+import ClientOnly from "../components/ClientOnly";
 import EmptyState from "../components/EmptyState";
 
-
-
 const TripsPage = async () => {
-    return <EmptyState title="Trips Page"/>;
+   const currentUser = await getCurrentUser();
+
+   if (!currentUser) {
+      return (
+         <ClientOnly>
+            <EmptyState title="Unauthorized" subTitle="Please Login" />
+         </ClientOnly>
+      );
+   }
+
+   try {
+      const reservations = await getReservations({ userId: currentUser.id });
+
+      if (reservations.length === 0) {
+         return (
+            <ClientOnly>
+               <EmptyState
+                  title="No trips found"
+                  subTitle="Looks like you haven't reserved any trips."
+               />
+            </ClientOnly>
+         );
+      }
+
+
+   } catch (error) {
+      console.error("Error fetching trips:", error);
+      return (
+         <ClientOnly>
+            <EmptyState
+               title="Something went wrong"
+               subTitle="Failed to load your trips. Please try again later."
+            />
+         </ClientOnly>
+      );
+   }
 };
+
+
 
 export default TripsPage;
